6da91c07f6018fcc056f916754d27169faec827a
lgma <lgmendez@gmail.com>
2013-08-11 11:04:08 -0500

    Commit inicial

---
+++ performance monitor web/js/menu.js
@@ -0,0 +1,24 @@
+<!--
+document.writeln('<TABLE BORDER="0" ALIGN="Center">');
+document.writeln('<TR>');
+document.writeln('<TD>');
+document.writeln('<a href="dashboard.html">Dashboard</a>');
+document.writeln('</TD>');
+document.writeln('<TD>');
+document.writeln('<a href="DOMs.html">DOMs</a>');
+document.writeln('</TD>');
+document.writeln('<TD>');
+document.writeln('<a href="RNC.html">RNC</a>');
+document.writeln('</TD>');
+document.writeln('<a href="PDSN.html">PDSN</a>');
+document.writeln('</TD>');
+document.writeln('<a href="Tools.html">Tools</a>');
+document.writeln('</TD>');
+document.writeln('<a href="Reports.html">Reports</a>');
+document.writeln('</TD>');
+document.writeln('<a href="Alerts.html">Alerts</a>');
+document.writeln('</TD>');
+
+document.writeln('</TR>');
+document.writeln('</TABLE>');
+//-->
